5c8b96f9ec6719e5f3b8f035e154bbbf73f82e87
cli <cli@mastercontrol.com>
2018-05-05 17:21:19 -0600

    made a bunch of legend of zelda themed changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,37 +12,96 @@ var currentYourScore;
 //add random number between 19 and 120 for target div
 initializeGame();
 
+//plays a sound when triggered
+function playRupee() {
+    if (losses < 4) {
+        var audio = document.getElementById("rupee");
+        var clone = audio.cloneNode(true);
+        clone.play();
+    }
+}
+
+function playWin() {
+    var audio = document.getElementById("win");
+    var clone = audio.cloneNode(true);
+    clone.play();
+}
+
+function playLose() {
+    var audio = document.getElementById("lose");
+    var clone = audio.cloneNode(true);
+    clone.play();
+}
+
+function playGameOver() {
+    var audio = document.getElementById("gameOver");
+    var clone = audio.cloneNode(true);
+    clone.play();
+}
+
 //uh...clicking img crystals will add the data-crystal value from the particular crystal
 //to the player's total score value
 $(".img-crystal").on("click", function () {
-    currentYourScore = $(".yourScoreNum").text();
-    currentYourScore = parseInt(currentYourScore) + parseInt($(this).attr("data-crystal"));
-    $(".yourScoreNum").text(currentYourScore);
-    //run a function to check if you win or lose
-    checkStatus();
+    if (losses < 4) {
+        currentYourScore = $(".yourScoreNum").text();
+        currentYourScore = parseInt(currentYourScore) + parseInt($(this).attr("data-crystal"));
+        $(".yourScoreNum").text(currentYourScore);
+        //run a function to check if you win or lose, wait for 10 milliseconds to allow DOM to update before alert takes over
+        setTimeout(function () { checkStatus(); }, 10);
+    }
 })
+
+function heartContainerCheck() {
+    if (losses === 1) {
+        $(".firstHeart").hide();
+    }
+    if (losses === 2) {
+        $(".secondHeart").hide();
+    }
+    if (losses === 3) {
+        $(".thirdHeart").hide();
+    }
+    if (losses === 4) {
+        $(".finalHeart").hide();
+    }
+}
+
+function endGame() {
+    alert("You've run out of lives. Please play again.")
+    playGameOver();
+}
 
 function checkStatus() {
     //check if the player score is equal to target score, if so, increase win, run reset
     //else check if the player score is gerater than target score, if so, increase loss, run reset
     //else do nothing.
     if (currentYourScore === targetNumber) {
+
         wins++;
         $(".win-counter").text(wins);
+        playWin();
         alert("Well done, you win! Your current win count is at: " + wins)
         initializeGame();
     }
     else if (currentYourScore > targetNumber) {
+        playLose();
         losses++;
         $(".loss-counter").text(losses);
+        heartContainerCheck();
+        playLose();
         alert("Oh no, you went over target and lost! Your current loss count is at: " + losses)
-        initializeGame();
+        if (losses !== 4) {
+            initializeGame();
+        } else {
+            endGame();
+        }
+
     } else {
         return;
     }
 }
 
-
+//resets everything, recalculates random numbers, gets game ready to run again
 function initializeGame() {
     currentYourScore = 0;
     $(".yourScoreNum").text(currentYourScore)
